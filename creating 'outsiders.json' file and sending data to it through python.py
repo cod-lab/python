342c3adc57a6067ae421cbf2509f1e227aec5b23
cod-lab <arihant806@gmail.com>
2020-03-19 05:42:55 +0530

    updated file 'creating json file...' to insert data into empty or non-existed file

--- creating 'outsiders.json' file and sending data to it through python.py
+++ creating 'outsiders.json' file and sending data to it through python.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 class person():
     country = "india"
@@ -17,38 +18,52 @@ class person():
         y=input("enter ur college name:\t")
         print("ur name is",x,"& ur college is",y)
         
-        z=input("\nenter ur country name:\t")
+        z=input("\nenter ur country name:\t")#+"\n"
+        #z+="\n"
         #print("x: ",type(x),"\tz: ",type(z))
+        
         if z.lower()!=self.country:
-            self.list1={}
-            self.n=1
-            self.n+=self.update_json('outsiders.json')  #--> for reading
-            #getting length of the dictionary by calling 'update_json' fn, Using 'read' fn to read the given json file to get the length of dictionary stored in that file
+            n=1
+            list1={}
+            file = 'outsiders.json'
+
+            #if os.path.isfile(file)                                        #also works
+            if os.path.exists(file) == False:                               #chk if file exists or not
+                print("file created:\t",file)
+                #if file doesn't exists then it is created at this point(even before calling fn' 'update_json' for reading file)
             
-            self.list1[self.n]={x:z}
+            n+=self.update_json(file,list1)  #--> for reading
+            #getting length of the dictionary by calling fn 'update_json', Using 'READ'(r) fn to read the given json file to get the length of dictionary stored in that file
+
+            list1[n]={x:z}#+"\n" #print("jvgjv\n"+a)
+            #list1+="\n"
             
-            self.update_json('outsiders.json')  #--> for writing
-            #calling 'update_json' fn to write data into json file, Using 'write' fn to write data in the dictionary in the given json file
-            print("data added -->\n",self.list1)
+            self.update_json(file,list1)  #--> for writing
+            #calling fn 'update_json' to write data into json file, Using 'WRITE'(w) fn to write data in the dictionary in the given json file
+            
+            print("data added -->\n",list1)
         else:
             print("\nok! u r from here only\n(no data added to json)\n")
-
-
-    def update_json(self,file):
+    
+    def update_json(self,file,list1):
         #print(self.list1)
         #print("json",type(self.list1))
         
-        #reading -->
         temp={}
-        with open(file,'r') as f:                           #load outsiders.json file into ram, File is opened in reading mode
-            temp=json.load(f)                               #print outsiders.json file data here        
+        if os.path.exists(file) and os.stat(file).st_size != 0:     #chk if file exists or not and file is empty or not by checking its size
+            #reading -->
+            with open(file,'r') as f:                               #load file 'outsiders.json' in READ(r) mode and creates file handler with name 'f' to handle file
+                temp=json.load(f)                                   #print file 'outsiders.json' data here and also stores in 'temp' variable
 
         #writing -->
-        temp.update(self.list1)
-        with open(file,'w') as f:                           #creates json file on hdd(same folder) if not exist already and open it internally
-            json.dump(temp,f)                               #add data into outsiders.json file
+        temp.update(list1)                                          #adding data into variable 'temp' in which file 'outsiders.json' data is already stored
+        with open(file,'w') as f:
+        #open file 'outsiders.json' in WRITE(w) mode and creates file handler with name 'f' to handle file.
+        #if file doesn't exist already then creates it automatically with the name given above and store it in the same folder    
+            json.dump(temp,f)
+            #add data of 'temp' variable into file 'outsiders.json' through file handler 'f' and overwrites previous data of the file
 
-        return len(temp)                                    #getting length of data(no. of keys or values) stoored in json file and returning it
+        return len(temp)                                            #getting length of data(no. of keys or values) stored in json file and returning it
         
 
 obj1 = person("rocky","mit",22,12312)
